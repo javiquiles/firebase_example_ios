dafc9cdf9a71727c8f17ca66d8646e4eec7cef11
Javier Quiles <javiquiles@hotmail.com>
2020-07-07 00:13:27 -0300

    Adding oulets and actions

---
+++ FirebaseExample/Home/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  FirebaseExample
+//
+//  Created by Javier Quiles on 06/07/2020.
+//  Copyright © 2020 Javier Quiles. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var providerLabel: UILabel!
+    @IBOutlet weak var logOutButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    @IBAction func logOutButtonAction(_ sender: Any) {
+    }
+
+}
